fcb8199b0047962ff16bffa10394d26d3961f74b
Shubham Sharma <shubham03901@gmail.com>
2025-05-04 00:41:25 +0530

    Modified DNSHeader Constructor to correctly handle header bytes arraybuffer

--- lib/headers.class.ts
+++ lib/headers.class.ts
@@ -1,17 +1,26 @@
 import type { DNSHeaderType } from "../types/headers";
 export class DNSHeader {
-	private headerBuffer: Uint16Array = new Uint16Array(12);
+	private headerBuffer: Uint16Array = new Uint16Array(6);
 
 	constructor(data: DNSHeaderType) {
-		this.headerBuffer.set([data.pid], 0);
-		this.headerBuffer.set(
-			[data.qr, data.opcode, data.aa, data.tc, data.ra, data.z, data.rcode],
-			2
-		);
-		this.headerBuffer.set([data.qdcount], 4);
-		this.headerBuffer.set([data.ancount], 6);
-		this.headerBuffer.set([data.nscount], 8);
-		this.headerBuffer.set([data.arcount], 10);
+		const flags =
+			(data.qr << 15) |
+			(data.opcode << 11) |
+			(data.aa << 10) |
+			(data.tc << 9) |
+			(data.rd << 8) |
+			(data.ra << 7) |
+			(data.z << 4) |
+			data.rcode;
+
+		this.headerBuffer.set([
+			data.pid,
+			flags,
+			data.qdcount,
+			data.ancount,
+			data.nscount,
+			data.arcount,
+		]);
 	}
 
 	public getHeaderBuffer(): Buffer {
